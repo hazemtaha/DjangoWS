02fdc2f1edd6a7a8824e1c03c8de065f95bd5e2f
asmaa <asmaa.elsayed00@gmial.com>
2016-03-31 12:34:59 +0200

    DB

--- WSServer/WSServer/settings.py
+++ WSServer/WSServer/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'blog',
+    'ws',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,9 +79,12 @@ WSGI_APPLICATION = 'WSServer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'HOST':'localhost',
+        'USER':'root',
+        'PASSWORD':'iti',
+         }
 }
 
 
--- WSServer/blog/models.py
+++ WSServer/blog/models.py
@@ -1,5 +1,43 @@
+# encoding: utf-8
 from __future__ import unicode_literals
-
+from django.contrib.auth.admin import User
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
 
-# Create your models here.
+
+
+class Post(models.Model):
+
+	uid=models.ForeignKey(User ,on_delete=models.CASCADE);
+	body=models.TextField()
+	def __unicode__(self):
+		return self.uid
+
+
+class App (models.Model):
+	app_name=models.CharField(max_length=255)
+	uri=models.URLField(max_length=255)
+	devID=models.ForeignKey(User ,on_delete=models.CASCADE);
+	clientID=models.IntegerField();
+
+	secretKey=models.IntegerField();
+	#rec_id=models.ForeignKey(Users ,default=1)
+
+
+
+class AccessToken(models.Model):
+	
+	appID=models.ForeignKey(App ,on_delete=models.CASCADE);
+	uid=models.ForeignKey(User ,on_delete=models.CASCADE);
+	exp_date=models.DateField();
+	refreshToken=models.IntegerField(null=True,blank=True);
+
+
+
+class Code(models.Model):
+	
+	appID=models.ForeignKey(App ,on_delete=models.CASCADE);
+	uid=models.ForeignKey(User ,on_delete=models.CASCADE);
+	code=models.IntegerField();
+
