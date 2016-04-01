8fa389856d799eb7efb5dcb6444ed0397245ca27
asmaa <asmaa.elsayed00@gmial.com>
2016-04-01 12:27:51 +0200

    DB

--- WSServer/WSServer/urls.py
+++ WSServer/WSServer/urls.py
@@ -13,11 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+import settings
 from django.conf.urls import url, include
 from django.contrib import admin
-
-import settings
-
 from blog import views
 
 urlpatterns = [
@@ -25,7 +23,7 @@ urlpatterns = [
     url(r'^blog/', include('blog.urls')),
     url(r'^ws/', include('ws.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')) ,
+
 
 ]
-STATIC_URL = '/static/'
--- WSServer/blog/urls.py
+++ WSServer/blog/urls.py
@@ -1,10 +1,13 @@
 
+
 from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
 
 urlpatterns = [
+
     url(r'^index$', views.index,name="index"),
     url(r'^login$', views.login,name="login"),
     url(r'^registration$', views.registration,name="registration"),
 ]
+STATIC_URL = '/static/'
